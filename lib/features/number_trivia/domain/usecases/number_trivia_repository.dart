e0a85aaa1b83f51af549d987288dc4178c75f8bf
alithegilfoyle <alimurtaza-sw17@muetkhp.edu.pk>
2021-08-25 18:09:48 +0500

    Add number trivia repository contract

---
+++ lib/features/number_trivia/domain/usecases/number_trivia_repository.dart
@@ -0,0 +1,8 @@
+import '../../../../core/error/failures.dart';
+import '../entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
+}
