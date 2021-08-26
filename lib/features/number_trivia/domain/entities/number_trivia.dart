ef6bd5d50ee191ce595c50a65e23fe8d05fea1ef
alithegilfoyle <alimurtaza-sw17@muetkhp.edu.pk>
2021-08-26 15:02:07 +0500

    Add number trivia repository contract after local copy lose

--- lib/features/number_trivia/domain/entities/number_trivia.dart
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -10,6 +10,5 @@ class NumberTrivia extends Equatable {
   }) : super();
 
   @override
-  // TODO: implement props
-  List<Object?> get props => throw UnimplementedError();
+  List<Object?> get props => [text, number];
 }
