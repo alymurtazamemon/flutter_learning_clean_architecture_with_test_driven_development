2f40bfbbd3e01df48855ad991d1fe79a6fda88b2
alithegilfoyle <alimurtaza-sw17@muetkhp.edu.pk>
2021-08-31 09:19:16 +0500

    Switch to internet_connection_checker from data_connection_checker due to null-safety issue

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -1,16 +1,17 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_clean_architecture_with_test_driven_development/core/network/network_info.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+class MockInternetConnectionChecker extends Mock
+    implements InternetConnectionChecker {}
 
 void main() {
   late NetworkInfoImpl networkInfo;
-  late MockDataConnectionChecker mockDataConnectionChecker;
+  late MockInternetConnectionChecker mockDataConnectionChecker;
 
   setUp(() {
-    mockDataConnectionChecker = MockDataConnectionChecker();
+    mockDataConnectionChecker = MockInternetConnectionChecker();
     networkInfo = NetworkInfoImpl(mockDataConnectionChecker);
   });
 
