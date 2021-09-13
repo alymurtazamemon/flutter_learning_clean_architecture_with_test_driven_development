41a33b2a6a6e8820ee983e733cbad648d0bd2b23
alithegilfoyle <alimurtaza-sw17@muetkhp.edu.pk>
2021-09-13 18:40:35 +0500

    Implement the top half, Message Display, Loding, and Trivia widgets

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_clean_architecture_with_test_driven_development/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_clean_architecture_with_test_driven_development/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 
 import '../../../../injection_container.dart';
@@ -27,11 +28,18 @@ class NumberTriviaPage extends StatelessWidget {
               BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
                 builder: (context, state) {
                   if (state is Empty) {
-                    return Container(
-                      // Third of the size of the screen
-                      height: MediaQuery.of(context).size.height / 3,
-                      // Message Text widgets / CircularLoadingIndicator
-                      child: Text('Start searching!'),
+                    return MessageDisplay(
+                      message: 'Start searching!',
+                    );
+                  } else if (state is Loading) {
+                    return LoadingWidget();
+                  } else if (state is Loaded) {
+                    return TriviaDisplay(
+                      numberTrivia: state.trivia,
+                    );
+                  } else if (state is Error) {
+                    return MessageDisplay(
+                      message: state.message,
                     );
                   }
                   throw UnimplementedError();
@@ -61,6 +69,92 @@ class NumberTriviaPage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MessageDisplay extends StatelessWidget {
+  final String message;
+
+  const MessageDisplay({
+    Key? key,
+    required this.message,
+  })  : assert(message != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // Third of the size of the screen
+      height: MediaQuery.of(context).size.height / 3,
+      // Message Text widgets / CircularLoadingIndicator
+      child: Center(
+        child: SingleChildScrollView(
+          child: Text(
+            message,
+            style: TextStyle(fontSize: 25),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class TriviaDisplay extends StatelessWidget {
+  final NumberTrivia numberTrivia;
+
+  const TriviaDisplay({
+    Key? key,
+    required this.numberTrivia,
+  })  : assert(numberTrivia != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      child: Column(
+        children: <Widget>[
+          // Fixed size, doesn't scroll
+          Text(
+            numberTrivia.number.toString(),
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // Expanded makes it fill in all the remaining space
+          Expanded(
+            child: Center(
+              // Only the trivia "message" part will be scrollable
+              child: SingleChildScrollView(
+                child: Text(
+                  numberTrivia.text,
+                  style: TextStyle(fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
